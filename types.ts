2f404898e2fc09d05de3380fb3181cbfdc78f045
mywaiting <ourwaiting@gmail.com>
2021-03-17 13:54:27 +0800

    add morw methods to types

--- types.ts
+++ types.ts
@@ -1,5 +1,8 @@
 
-
+import {
+    OID_TYPE_CODE,
+    OID_TYPE_NAME
+} from './const.ts'
 import {
     BufferReader,
     BufferWriter,
@@ -66,9 +69,11 @@ export type TID = [BigInt, BigInt]
 export type Timestamp = Date | number
 
 
-// 
-// type reader and writer
-// 
+/**
+ * type reader and writer
+ * codes origin from https://github.com/denodrivers/postgres
+ * codes has been rewrite/rename
+ */
 
 const TYPE_DATE_BC_REGEXP = /BC$/
 const TYPE_DATE_REGEXP = /^(\d{1,})-(\d{2})-(\d{2})$/
@@ -80,8 +85,160 @@ export class TypeReader {
 
     constructor() {}
 
-    decode(value: Uint8Array, format: 'text' | 'binary') {
-        
+    decode(value: Uint8Array, oid: OID_TYPE_CODE, format: 'text' | 'binary' = 'text') {
+        if (format === 'binary') {
+            throw new Error(`not support binary type decode now`)
+        } else if (format === 'text') {
+            const strValue = decode(value)
+            switch (oid) {
+                case OID_TYPE_CODE.bpchar:
+                case OID_TYPE_CODE.char:
+                case OID_TYPE_CODE.cidr:
+                case OID_TYPE_CODE.float4:
+                case OID_TYPE_CODE.float8:
+                case OID_TYPE_CODE.inet:
+                case OID_TYPE_CODE.macaddr:
+                case OID_TYPE_CODE.name:
+                case OID_TYPE_CODE.numeric:
+                case OID_TYPE_CODE.oid:
+                case OID_TYPE_CODE.regclass:
+                case OID_TYPE_CODE.regconfig:
+                case OID_TYPE_CODE.regdictionary:
+                case OID_TYPE_CODE.regnamespace:
+                case OID_TYPE_CODE.regoper:
+                case OID_TYPE_CODE.regoperator:
+                case OID_TYPE_CODE.regproc:
+                case OID_TYPE_CODE.regprocedure:
+                case OID_TYPE_CODE.regrole:
+                case OID_TYPE_CODE.regtype:
+                case OID_TYPE_CODE.text:
+                case OID_TYPE_CODE.time:
+                case OID_TYPE_CODE.timetz:
+                case OID_TYPE_CODE.uuid:
+                case OID_TYPE_CODE.varchar:
+                case OID_TYPE_CODE.void:
+                    return strValue
+                case OID_TYPE_CODE._bpchar:
+                case OID_TYPE_CODE._char:
+                case OID_TYPE_CODE._cidr:
+                case OID_TYPE_CODE._float4:
+                case OID_TYPE_CODE._float8:
+                case OID_TYPE_CODE._inet:
+                case OID_TYPE_CODE._macaddr:
+                case OID_TYPE_CODE._name:
+                case OID_TYPE_CODE._numeric:
+                case OID_TYPE_CODE._oid:
+                case OID_TYPE_CODE._regclass:
+                case OID_TYPE_CODE._regconfig:
+                case OID_TYPE_CODE._regdictionary:
+                case OID_TYPE_CODE._regnamespace:
+                case OID_TYPE_CODE._regoper:
+                case OID_TYPE_CODE._regoperator:
+                case OID_TYPE_CODE._regproc:
+                case OID_TYPE_CODE._regprocedure:
+                case OID_TYPE_CODE._regrole:
+                case OID_TYPE_CODE._regtype:
+                case OID_TYPE_CODE._text:
+                case OID_TYPE_CODE._time:
+                case OID_TYPE_CODE._timetz:
+                case OID_TYPE_CODE._uuid:
+                case OID_TYPE_CODE._varchar:
+                    return this._stringArray(strValue)
+
+                case OID_TYPE_CODE.int2:
+                case OID_TYPE_CODE.int4:
+                case OID_TYPE_CODE.xid:
+                    return this._int(strValue)
+                case OID_TYPE_CODE._int2:
+                case OID_TYPE_CODE._int4:
+                case OID_TYPE_CODE._xid:
+                    return this._intArray(strValue)
+                
+                case OID_TYPE_CODE.bool:
+                    return this._boolean(strValue)
+                case OID_TYPE_CODE._bool:
+                    return this._booleanArray(strValue)
+                
+                case OID_TYPE_CODE.box:
+                    return this._box(strValue)
+                case OID_TYPE_CODE._box:
+                    return this._boxArray(strValue)
+
+                case OID_TYPE_CODE.bytea:
+                    return this._bytea(strValue)
+                case OID_TYPE_CODE._bytea:
+                    return this._byteaArray(strValue)
+                
+                case OID_TYPE_CODE.circle:
+                    return this._circle(strValue)
+                case OID_TYPE_CODE._circle:
+                    return this._circleArray(strValue)
+
+                case OID_TYPE_CODE.date:
+                    return this._date(strValue)
+                case OID_TYPE_CODE._date:
+                    return this._dateArray(strValue)
+
+                case OID_TYPE_CODE.int8:
+                    return this._bigint(strValue)
+                case OID_TYPE_CODE._int8:
+                    return this._bigintArray(strValue)
+                
+                case OID_TYPE_CODE.json:
+                case OID_TYPE_CODE.jsonb:
+                    return this._json(strValue)
+                case OID_TYPE_CODE._json:
+                case OID_TYPE_CODE._jsonb:
+                    return this._jsonArray(strValue)
+
+                case OID_TYPE_CODE.line:
+                    return this._line(strValue)
+                case OID_TYPE_CODE._line:
+                    return this._lineArray(strValue)
+
+                case OID_TYPE_CODE.lseg:
+                    return this._lineSegment(strValue)
+                case OID_TYPE_CODE._lseg:
+                    return this._lineSegmentArray(strValue)
+                
+                case OID_TYPE_CODE.path:
+                    return this._path(strValue)
+                case OID_TYPE_CODE._path:
+                    return this._pathArray(strValue)
+                
+                case OID_TYPE_CODE.point:
+                    return this._point(strValue)
+                case OID_TYPE_CODE._point:
+                    return this._pointArray(strValue)
+                
+                case OID_TYPE_CODE.polygon:
+                    return this._polygon(strValue)
+                case OID_TYPE_CODE._polygon:
+                    return this._polygonArray(strValue)
+                
+                case OID_TYPE_CODE.tid:
+                    return this._tid(strValue)
+                case OID_TYPE_CODE._tid:
+                    return this._tidArray(strValue)
+                
+                case OID_TYPE_CODE.timestamp:
+                case OID_TYPE_CODE.timestamptz:
+                    return this._datetime(strValue)
+                case OID_TYPE_CODE._timestamp:
+                case OID_TYPE_CODE._timestamptz:
+                    return this._datetimeArray(strValue)
+                
+                default:
+                    // A separate category for not handled values
+                    // They might or might not be represented correctly as strings,
+                    // returning them to the user as raw strings allows them to parse
+                    // them as they see fit
+                    return strValue
+
+            }
+        } else {
+            throw new Error(`unknown format type decode: ${format}`)
+        }
     }
 
     _bigint(value: string): BigInt {
@@ -114,7 +271,44 @@ export class TypeReader {
         return new ArrayParser(value, this._box).parse()
     }
 
-    _bytea(value: string): Uint8Array {}
+    _bytea(value: string): Uint8Array {
+        // byta Escape
+        if (TYPE_HEX_PREFIX_REGEX.test(value)) {
+            const bytes = []
+            let i = 0, k = 0
+            // loop 
+            while (i < value.length) {
+                if (value[i] !== "\\") {
+                    bytes.push(value.charCodeAt(i))
+                    ++i
+                } else {
+                    if (/[0-7]{3}/.test(value.substr(i + 1, 3))) {
+                        bytes.push(parseInt(value.substr(i + 1, 3), 8))
+                        i += 4
+                    } else {
+                        let backslashes = 1
+                        while (i + backslashes < value.length && value[i + backslashes] === "\\") {
+                            backslashes++
+                        }
+                        for (k = 0; k < Math.floor(backslashes / 2); ++k) {
+                            bytes.push(0x5c) // 92 === 0x5c === '\\'.charCodeAt(0), backslash
+                        }
+                        i += Math.floor(backslashes / 2) * 2
+                    }
+                }
+            }
+            return new Uint8Array(bytes)
+        // bytea HEX
+        } else {
+            value = value.slice(2)
+            const bytes = new Uint8Array(value.length / 2)
+            for (let i = 0, j = 0; i < value.length; i += 2, j++) {
+                // parseInt(xxx, 16) means 16 radix, return like 0x13
+                bytes[j] = parseInt(value[i] + value[i + 1], 16)
+            }
+            return bytes
+        }
+    }
 
     _byteaArray(value: string) {
         return new ArrayParser(value, this._bytea).parse()
@@ -245,29 +439,93 @@ export class TypeReader {
         };
     }
 
-    _lineSegmentArray(value: string) {}
+    _lineSegmentArray(value: string) {
+        return new ArrayParser(value, this._lineSegment).parse()
+    }
 
-    _path(value: string): Path {}
+    _path(value: string): Path {
+        // Split on commas that are not inside parantheses
+        // since encapsulated commas are separators for the point coordinates
+        const points = value.substring(1, value.length - 1).split(/,(?![^(]*\))/)
+        return points.map(this._point)
+    }
 
-    _pathArray(value: string) {}
+    _pathArray(value: string) {
+        return new ArrayParser(value, this._path).parse()
+    }
 
-    _point(value: string): Point {}
+    _point(value: string): Point {
+        const [x, y] = value.substring(1, value.length - 1).split(",")
 
-    _pointArray(value: string) {}
+        if (Number.isNaN(parseFloat(x)) || Number.isNaN(parseFloat(y))) {
+            throw new Error(
+                `Invalid point value: "${Number.isNaN(parseFloat(x)) ? x : y}"`
+            );
+        }
 
-    _polygon(value: string): Polygon {}
+        return {
+            x: x as Float8,
+            y: y as Float8
+        }
+    }
 
-    _polygonArray(value: string) {}
+    _pointArray(value: string) {
+        return new ArrayParser(value, this._point).parse()
+    }
 
-    _string(value: string): string {}
+    _polygon(value: string): Polygon {
+        return this._path(value)
+    }
 
-    _stringArray(value: string) {}
+    _polygonArray(value: string) {
+        return new ArrayParser(value, this._polygon).parse()
+    }
 
-    _timezoneOffset(value: string): number | null {}
+    _string(value: string): string {
+        return String(value)
+    }
 
-    _tid(value: string): TID {}
+    _stringArray(value: string) {
+        if (!value) return null
+        return new ArrayParser(value, this._string).parse()
+    }
 
-    _tidArray(value: string): TID[] {}
+    _timezoneOffset(value: string): number | null {
+        // get rid of date part as TIMEZONE_RE would match '-MM` part
+        value = value.split(' ')[1]
+        const matches = TYPE_TIMEZONE_REGEXP.exec(value)
+        if (!matches) {
+            return null
+        }
+
+        const prefix = matches[1]
+        // special, zulu timezone is zero, UTC === 0
+        if (prefix === 'Z') {
+            return 0
+        }
+
+        // in JS timezone offsets are reversed, ie. timezones
+        // that are "positive" (+01:00) are represented as negative
+        // offsets and vice-versa
+        const sign = prefix === '-' ? 1 : -1
+
+        const hours = parseInt(matches[2], 10)
+        const minutes = parseInt(matches[3] || "0", 10)
+        const seconds = parseInt(matches[4] || "0", 10)
+
+        const offset = hours * 3600 + minutes * 60 + seconds
+
+        return sign * offset * 1000
+    }
+
+    _tid(value: string): TID {
+        const [x, y] = value.substring(1, value.length - 1).split(",")
+        return [BigInt(x), BigInt(y)]
+    }
+
+    _tidArray(value: string) {
+        return new ArrayParser(value, this._tid).parse()
+    }
 }
 
 
@@ -299,11 +557,79 @@ export class TypeWriter {
         }
     }
 
-    _array(value: Array<unknown>): string {}
+    _array(value: Array<unknown>): string {
+        const encodedArray = ['{']
+        value.forEach((element, index) => {
+            if (index > 0) {
+                encodedArray.push(',')
+            }
+            if (element === null || typeof element === 'undefined') {
+                encodedArray.push('NULL')
 
-    _byte(value: Uint8Array): string {}
+            } else if (Array.isArray(element)) {
+                encodedArray.concat(this._array(element))
 
-    _date(value: Date): string {}
+            } else if (element instanceof Uint8Array) {
+                throw new Error(`buffer could encoded now`)
+
+            } else {
+                const encodedElement = this.encode(element)
+                // deno-lint-ignore no-explicit-any
+                const strValue = (encodedElement as any).toString()
+                const escapedValue = strValue.replace(/\\/g, "\\\\").replace(/"/g, '\\"')
+                encodedArray.push(`"${escapedValue}"`)
+            }
+        })
+        encodedArray.push('}')
+        return encodedArray.join('')
+    }
+
+    _byte(value: Uint8Array): string {
+        const strValue = Array.from(value).map((val) => {
+            // number.toString(16), means hexadecimal string output
+            // number.toString(radix)
+            return val < 10 ? `0${val.toString(16)}` : val.toString(16)
+
+        }).join("")
+
+        return `\\x${strValue}`
+    }
+
+    _date(value: Date): string {
+        // construct iso datetime string
+        const year  = this.padding(value.getFullYear(), 4)
+        const month = this.padding(value.getMonth() + 1, 2) // new Date().getMonth()
+        const day   = this.padding(value.getDate(), 2)
+        const hour  = this.padding(value.getHours(), 2)
+        const min   = this.padding(value.getMinutes(), 2)
+        const sec   = this.padding(value.getSeconds(), 2)
+        const ms    = this.padding(value.getMilliseconds(), 3)
+
+        const encodedDate = `${year}-${month}-${day}T${hour}:${min}:${sec}.${ms}`
+
+        // construct iso timezone string
+        const offset    = value.getTimezoneOffset()
+        const sign      = offset > 0 ? "-" : "+"
+        const absOffset = Math.abs(offset)
+        const tzHours   = this.padding(Math.floor(absOffset / 60), 2)
+        const tzMinutes = this.padding(Math.floor(absOffset % 60), 2)
+
+        const encodedTz = `${sign}${tzHours}:${tzMinutes}`
+
+        return encodedDate + encodedTz;
+    }
+
+    /**
+     * padding a number with capacity(how much) zero into value
+     * for example, padding(123, 6) === '000123'
+     */
+    padding(value: number, capacity: number): string {
+        let strValue = String(value)
+        while (strValue.length < capacity) {
+            strValue = '0' + strValue
+        }
+        return strValue
+    }
 
     
 }
@@ -440,16 +766,4 @@ export class ArrayParser<ResultT> {
             }
         }
     }
-}
-
-export function parseArray(elements: string): ArrayParserResult<string>
-export function parseArray<ResultT>(
-    elements: string,
-    transformer: ArrayParserTransformer<ResultT>
-): ArrayParserResult<ResultT>
-export function parseArray(
-    elements: string,
-    transformer = (value: string): string => String(value)
-) {
-    return new ArrayParser(elements, transformer).parse()
 }
