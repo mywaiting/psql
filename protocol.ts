2f761c81d7fa0e2ae014629d5a64d24caf4fa183
mywaiting <ourwaiting@gmail.com>
2021-03-22 00:15:31 +0800

    add CommandComplete parse for commandTag

--- protocol.ts
+++ protocol.ts
@@ -205,15 +205,43 @@ export class CloseCompleteReader extends PacketReader {
     }
 }
 
+
+const COMMAND_TAG_REGEXP = /^([A-Za-z]+)(?: (\d+))?(?: (\d+))?/
+
 export class CommandCompleteReader extends PacketReader {
     
+    /**
+     * CommandComplete returns:
+     * For an INSERT command, the tag is INSERT oid rows, where rows is the number of rows inserted. oid used to be the object ID of the inserted row if rows was 1 and the target table had OIDs, but OIDs system columns are not supported anymore; therefore oid is always 0.
+     * For a DELETE command, the tag is DELETE rows where rows is the number of rows deleted.
+     * For an UPDATE command, the tag is UPDATE rows where rows is the number of rows updated.
+     * For a SELECT or CREATE TABLE AS command, the tag is SELECT rows where rows is the number of rows retrieved.
+     * For a MOVE command, the tag is MOVE rows where rows is the number of rows the cursor's position has been changed by.
+     * For a FETCH command, the tag is FETCH rows where rows is the number of rows that have been retrieved from the cursor.
+     * For a COPY command, the tag is COPY rows where rows is the number of rows copied. (Note: the row count appears only in PostgreSQL 8.2 and later.)
+     */
     read(reader: BufferReader) {
-        const data = reader.readSlice(0x00)
-        return {
-            name: this.name,
-            code: this.code,
-            length: this.length,
-            data: data
+        const commandTag = reader.readSlice(0x00)
+        const metches = COMMAND_TAG_REGEXP.exec(commandTag)
+        if (metches) {
+            return {
+                name: this.name,
+                code: this.code,
+                length: this.length,
+                commandTag: commandTag,
+                // INSERT | DELETE | UPDATE | DELECT | MOVE | FETCH | COPY
+                command: metches[1],
+                // metches[3] = command oid rows
+                // metches[2] = command rows
+                count: metches[3] ? parseInt(metches[3], 10) : parseInt(metches[2], 10)
+            }
+        } else {
+            return {
+                name: this.name,
+                code: this.code,
+                length: this.length,
+                commandTag: commandTag
+            }
         }
     }
 }
