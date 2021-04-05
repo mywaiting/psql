7a5985e2a095e82791862026791de108c2357ce3
mywaiting <ourwaiting@gmail.com>
2021-04-05 16:47:26 +0800

    remove deferred from deps and re-added in deferred

--- deferred.ts
+++ deferred.ts
@@ -1,6 +1,4 @@
-import {
-    deferred
-} from './deps.ts'
+
 
 
 /**
@@ -11,6 +9,14 @@ export interface Deferred<T> extends Promise<T> {
     resolve: (value?: T | PromiseLike<T>) => void
     // deno-lint-ignore no-explicit-any
     reject: (reason?: any) => void
+}
+
+export function deferred<T>(): Deferred<T> {
+    let methods
+    const promise = new Promise<T>((resolve, reject): void => {
+        methods = { resolve, reject }
+    })
+    return Object.assign(promise, methods) as Deferred<T>
 }
 
 
